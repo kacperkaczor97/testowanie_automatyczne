ea191d9d498545a8aefb38f1987269581ba787c4
kacperkaczor97 <kacperkaczor97@gmail.com>
2020-11-04 17:14:49 +0100

    Poprawa Testow

--- project1/src/testy/testy.java
+++ project1/src/testy/testy.java
@@ -6,29 +6,48 @@ import org.junit.jupiter.api.Test;
 
 import rownanie.Delta;
 
+import rownanie.X;
+
+import rownanie.X1;
+
+import rownanie.X2;
+
 class testy {
 
-	Delta rownanie = new Delta();
+	Delta rownanie1 = new Delta();
+	
+	X rownanie2 = new X();
+	
+	X1 rownanie3 = new X1();
+	
+	X2 rownanie4 = new X2();
 	
 	@Test
     public void testrownanie1() {
          
-        double resultdelta = rownanie.Rownanie(2, 5, 6);
+        double resultdelta = rownanie1.Rownanie(2, 5, 6);
         assertEquals(-23.0, resultdelta);
     }
 	
 	@Test
     public void testrownanie2() {
          
-        double resultx = rownanie.Rownanie(3, 4, 5);
-        assertEquals(-44.0, resultx);
+        double resultx = rownanie2.ZmiennaX(1, -6, 9);
+        assertEquals(3.0, resultx);
     }
 	
 	 @Test
 	    public void testrownanie3() {
 		 
-		 double resultx1 = rownanie.Rownanie(2, 5, 6);
-	    	assertEquals(-23.0, resultx1);
+		 double resultx1 = rownanie3.ZmiennaX1(1, 12, 13);
+	    	assertEquals(-1.2041684766872809, resultx1);
+	    }
+	 
+	 @Test
+	    public void testrownanie4() {
+		 
+		 double resultx1 = rownanie4.ZmiennaX2(1, 12, 15);
+	    	assertEquals(-10.582575694955839, resultx1);
 	    }
 
 }
